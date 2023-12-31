c34913adc171ee5a1065adf72a15d7b66fe17d2a
Ciurchil-Giurgiu Celia <celia.ciurchil@student.unitbv.ro>
2023-12-31 12:23:59 +0200

    Am definit clasa Camera cu membri ei.

--- Tancodrom/Tancodrom/Tancodrom.cpp
+++ Tancodrom/Tancodrom/Tancodrom.cpp
@@ -25,6 +25,54 @@
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+enum ECameraMovementType
+{
+	UNKNOWN,
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN
+};
+
+class Camera
+{
+private:
+    // Default camera values
+    const float zNEAR = 0.1f;
+    const float zFAR = 500.f;
+    const float YAW = -90.0f;
+    const float PITCH = 0.0f;
+    const float FOV = 45.0f;
+    glm::vec3 startPosition;
+
+protected:
+    const float cameraSpeedFactor = 2.5f;
+    const float mouseSensitivity = 0.1f;
+
+    // Perspective properties
+    float zNear;
+    float zFar;
+    float FoVy;
+    int width;
+    int height;
+    bool isPerspective;
+
+    glm::vec3 position;
+    glm::vec3 forward;
+    glm::vec3 right;
+    glm::vec3 up;
+    glm::vec3 worldUp;
+
+    // Euler Angles
+    float yaw;
+    float pitch;
+
+    bool bFirstMouseMove = true;
+    float lastX = 0.f, lastY = 0.f;
+};
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
