0b69930fbb96ddfdcae999fd6456319bd3ea56e4
Casigas <casian.gocan@student.unitbv.ro>
2024-01-02 11:42:41 -0500

    Toti setter-ii necesari pentru clasa shader au fost creati!

--- Tancodrom/Tancodrom/Tancodrom.cpp
+++ Tancodrom/Tancodrom/Tancodrom.cpp
@@ -253,7 +253,27 @@ public:
     unsigned int loc_view_matrix;
     unsigned int loc_projection_matrix;
 
-    
+    //Setteri
+    void SetInt(const std::string& name, int value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+    }
+    void SetFloat(const std::string& name, float value) const
+    {
+        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+    }
+    void SetVec3(const std::string& name, const glm::vec3& value) const
+    {
+        glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+    }
+    void SetVec3(const std::string& name, float x, float y, float z) const
+    {
+        glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+    }
+    void SetMat4(const std::string& name, const glm::mat4& mat) const
+    {
+        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    }
 private:
     unsigned int ID;
 };
